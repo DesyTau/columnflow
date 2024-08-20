Fix to the normalization weights, pu weights and plotting

columnflow/production/normalization.py
@@ -189,10 +189,8 @@ def normalization_weights(self: Producer, events: ak.Array, **kwargs) -> ak.Arra
             f"process_id field contains id(s) {invalid_ids} for which no cross sections were "
             f"found; process ids with cross sections: {self.xs_process_ids}",
         )
-
     # read the weight per process (defined as lumi * xsec / sum_weights) from the lookup table
     process_weight = np.squeeze(np.asarray(self.process_weight_table[0, process_id].todense()))
-
     # compute the weight and store it
     norm_weight = events.mc_weight * process_weight
     events = set_ak_column(events, self.weight_name, norm_weight, value_type=np.float32)
@@ -263,7 +261,6 @@ def normalization_weights_setup(
             MergeSelectionStats.merge_counts(merged_selection_stats, stats)
     else:
         merged_selection_stats = normalization_selection_stats[self.dataset_inst.name]
-
     # determine all proceses at any depth in the stitching datasets
     process_insts = {
         process_inst
@@ -311,6 +308,7 @@ def normalization_weights_setup(
                 )
         inclusive_xsec = inclusive_proc.get_xsec(self.config_inst.campaign.ecm).nominal
         for process_id, br in branching_ratios.items():
+            
             sum_weights = merged_selection_stats["sum_mc_weight_per_process"][str(process_id)]
             process_weight_table[0, process_id] = lumi * inclusive_xsec * br / sum_weights
     else:
@@ -318,12 +316,17 @@ def normalization_weights_setup(
             if self.config_inst.campaign.ecm not in process_inst.xsecs.keys():
                 continue
             sum_weights = merged_selection_stats["sum_mc_weight_per_process"][str(process_inst.id)]
+            #quick fix that need to be fixed
+            ################################
+            #n_evt_per_file = /self.dataset_inst.n_files
+            sum_weights = self.dataset_inst.n_events
+            ################################
             xsec = process_inst.get_xsec(self.config_inst.campaign.ecm).nominal
             process_weight_table[0, process_inst.id] = lumi * xsec / sum_weights
 
+
     self.process_weight_table = process_weight_table
     self.xs_process_ids = set(self.process_weight_table.rows[0])
-
 
 @normalization_weights.init
 def normalization_weights_init(self: Producer) -> None: